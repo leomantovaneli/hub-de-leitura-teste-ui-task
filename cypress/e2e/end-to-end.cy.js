f5d16097aec22b73a8001937091ecdbbbd3a6a9e
Leonardo Mantovaneli <leonardo.mantovaneli@outlook.com>
2026-05-20 08:30:37 -0300

    Exercício EBAC E2E

--- cypress/e2e/end-to-end.cy.js
+++ cypress/e2e/end-to-end.cy.js
@@ -1,28 +1,33 @@
+import { faker } from '@faker-js/faker';
+
 describe('Testes End To End do fluxo de cadastro e login', () => {
 
-    /* 
-    Testes End To End ou Testes de ponta a ponta, ligam uma série de funcionalidades de um sistema,
-    simulando o comportamento do usuário final. Esses testes verificam se diferentes partes do sistema
-    funcionam corretamente quando integradas, garantindo que o fluxo completo de uma funcionalidade
-    funcione como esperado.
-    Aqui iremos criar um teste end to end que cobre o fluxo de cadastro e login de um usuário em um sistema web.
-    Em apenas um teste, ou seja, em um único "it", iremos:
-    1. Acessar a página de cadastro.
-    2. Preencher o formulário de cadastro com dados válidos.
-    3. Submeter o formulário e verificar se o cadastro foi bem-sucedido.
-    4. Acessar a página de login.
-    5. Preencher o formulário de login com as credenciais do usuário recém-cadastrado.
-    6. Submeter o formulário de login e verificar se o login foi bem-sucedido.
+    })
 
-    Use as boas práticas aprendidas até agora para estruturar o teste.
-    */
 
-    beforeEach(() => {
-        // Configurações iniciais, se necessário
+    it('Deve cadastrar um usuário e realizar login com as mesmas credenciais', () => {
+        const usuario = {
+            nome: faker.person.fullName(),
+            email: faker.internet.email(),
+            telefone: faker.phone.number('## #####-####'),
+            senha: 'TesteEBAC@123',
+            confirmarSenha: 'TesteEBAC@123'
+        };
+        cy.preencherCadastro(
+            usuario.nome,
+            usuario.email,
+            usuario.telefone,
+            usuario.senha,
+            usuario.confirmarSenha
+        )
+        //Resultado Esperado: Cadastro realizado com sucesso e redirecionamento para dashboard
+        cy.get('.user-actions > .btn-outline-danger').click()
+        //Resultado esperado: Logout realizado com sucesso e redirecionamento para login
+        cy.url().should('include', 'login.html')
+        cy.login(
+            usuario.email,
+            usuario.senha
+        )
+        //Resultado esperado: Login realizado com sucesso e redirecionamento para dashboard
+
     });
-
-
-    it('Deve fazer o cadastro e validar o login com o usuário cadastrado', () => {
-        // Criar todo o fluxo aqui dentro deste único "it"
-    });
-});
